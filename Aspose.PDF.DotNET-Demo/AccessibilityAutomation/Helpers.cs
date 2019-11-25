d7e8023945a6af42eebad5b5139ecbdce29bbec0
Ben Hall <3179852+benbhall@users.noreply.github.com>
2019-11-25 18:57:35 +0000

    Code sample for tagged PDF

---
+++ Aspose.PDF.DotNET-Demo/AccessibilityAutomation/Helpers.cs
@@ -0,0 +1,58 @@
+﻿using Aspose.Pdf;
+using Aspose.Pdf.Text;
+using System.IO;
+
+namespace Automation
+{
+    public static class Helpers
+    {
+        /// <summary>
+        /// Generate a non- PDF/UA compliant PDF for use in demo
+        /// </summary>
+        /// <param name="outputFilePath">Full output path including filename with extension</param>
+        public static void CreateDemoNonCompliantPdfFile(string outputFilePath)
+        {
+            var pdfDoc = new Document();
+            pdfDoc.Pages.Add();
+            var pageOne = pdfDoc.Pages[1];
+
+            #region Add image
+            int lowerLeftX = 100;
+            int lowerLeftY = 680;
+            int upperRightX = 200;
+            int upperRightY = 780;
+
+            FileStream imageStream = new FileStream("aspose.png", FileMode.Open);
+            pageOne.Resources.Images.Add(imageStream);
+            pageOne.Contents.Add(new Aspose.Pdf.Operators.GSave());
+            Aspose.Pdf.Rectangle rectangle = new Aspose.Pdf.Rectangle(lowerLeftX, lowerLeftY, upperRightX, upperRightY);
+            Matrix matrix = new Matrix(new double[] { rectangle.URX - rectangle.LLX, 0, 0, rectangle.URY - rectangle.LLY, rectangle.LLX, rectangle.LLY });
+            pageOne.Contents.Add(new Aspose.Pdf.Operators.ConcatenateMatrix(matrix));
+            XImage ximage = pageOne.Resources.Images[pageOne.Resources.Images.Count];
+            pageOne.Contents.Add(new Aspose.Pdf.Operators.Do(ximage.Name));
+            pageOne.Contents.Add(new Aspose.Pdf.Operators.GRestore());
+            #endregion
+
+            #region Add text fragments
+            var textFragmentOne = new TextFragment("Some large indigo text for our header.");
+            textFragmentOne.Position = new Position(100, 800);
+            textFragmentOne.TextState.FontSize = 24;
+            textFragmentOne.TextState.Font = FontRepository.FindFont("TimesNewRoman");
+            textFragmentOne.TextState.ForegroundColor = Aspose.Pdf.Color.FromRgb(System.Drawing.Color.Indigo);
+
+            var textFragmentTwo = new TextFragment("Some smaller blue text in a non-tagged document.");
+            textFragmentTwo.Position = new Position(100, 660);
+            textFragmentTwo.TextState.FontSize = 12;
+            textFragmentTwo.TextState.Font = FontRepository.FindFont("TimesNewRoman");
+            textFragmentTwo.TextState.ForegroundColor = Aspose.Pdf.Color.FromRgb(System.Drawing.Color.Blue);
+
+            var textBuilder = new TextBuilder(pageOne);
+            textBuilder.AppendText(textFragmentOne);
+            textBuilder.AppendText(textFragmentTwo);
+            #endregion
+
+            pdfDoc.Save(outputFilePath);
+        }
+
+    }
+}
---
+++ Aspose.PDF.DotNET-Demo/AccessibilityAutomation/Program.cs
@@ -0,0 +1,104 @@
+﻿using Aspose.Pdf;
+using Aspose.Pdf.LogicalStructure;
+using Aspose.Pdf.Tagged;
+using Aspose.Pdf.Text;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Automation
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputFileName = "non-compliant.pdf";
+            var outputFileName = "compliant.pdf";
+
+            // Use some helper functions to create an example non-PDf/UA-compliant PDF
+            Helpers.CreateDemoNonCompliantPdfFile(inputFileName);
+            using (var d = new Document(inputFileName))
+            {
+                bool isValid = d.Validate("non-compliant-validation-log.xml", Aspose.Pdf.PdfFormat.PDF_UA_1);
+            }
+
+            var originalDocument = new Document(inputFileName);
+            var pageOne = originalDocument.Pages[1];
+
+            // Create our new tagged document
+            var taggedDocument = new Document();
+            ITaggedContent taggedContent = taggedDocument.TaggedContent;
+            StructureElement rootElement = taggedContent.RootElement;
+
+            // Set meta data required by PDF/UA
+            taggedContent.SetTitle("Our compliant document.");
+            taggedContent.SetLanguage("en-US");
+
+            // Extract from original PDF and create new structured elements for tagged PDF
+            HeaderElement h1 = ExtractToHeaderElement(taggedContent, pageOne, 1, 1);
+            ParagraphElement p = ExtractToParagraphElement(taggedContent, pageOne, 2);
+            FigureElement figureElement = ExtractToFigureElement(taggedContent, pageOne, 1);
+
+            // Append to new tagged content in desired order, which will build structure tree up from the 'root'
+            rootElement.AppendChild(h1);
+            rootElement.AppendChild(figureElement);
+            rootElement.AppendChild(p);
+
+            // Save and validate the compliant PDF
+            taggedDocument.Save(outputFileName);
+            using (var d = new Document(outputFileName))
+            {
+                bool isValid = d.Validate("compliant-validation-log.xml", Aspose.Pdf.PdfFormat.PDF_UA_1);
+            }
+        }
+
+        private static HeaderElement ExtractToHeaderElement(ITaggedContent taggedContent, Page page, int textIndex, int headerLevel = 1)
+        {
+            TextFragmentAbsorber textFragmentAbsorber = new TextFragmentAbsorber();
+            page.Accept(textFragmentAbsorber);
+            TextFragment originalHeaderText = textFragmentAbsorber.TextFragments[textIndex];
+
+            HeaderElement h1 = taggedContent.CreateHeaderElement(headerLevel);
+            h1.StructureTextState.ForegroundColor = originalHeaderText.TextState.ForegroundColor;
+            Font headerFont = FontRepository.FindFont(originalHeaderText.TextState.Font.FontName);
+            headerFont.IsEmbedded = true;
+            h1.StructureTextState.Font = headerFont;
+            h1.SetText(originalHeaderText.Text);
+
+            return h1;
+        }
+
+        private static ParagraphElement ExtractToParagraphElement(ITaggedContent taggedContent, Page page, int textIndex)
+        {
+            TextFragmentAbsorber textFragmentAbsorber = new TextFragmentAbsorber();
+            page.Accept(textFragmentAbsorber);
+
+            TextFragment originalText = textFragmentAbsorber.TextFragments[textIndex];
+
+            ParagraphElement p = taggedContent.CreateParagraphElement();
+            p.StructureTextState.ForegroundColor = originalText.TextState.ForegroundColor;
+            Font paraFont = FontRepository.FindFont(originalText.TextState.Font.FontName);
+            paraFont.IsEmbedded = true;
+            p.StructureTextState.Font = paraFont;
+            p.SetText(originalText.Text);
+
+            return p;
+        }
+
+        private static FigureElement ExtractToFigureElement(ITaggedContent taggedContent, Page page, int imageIndex)
+        {
+            ImagePlacementAbsorber imagePlacementAbsorber = new ImagePlacementAbsorber();
+            page.Accept(imagePlacementAbsorber);
+            XImage xImage = imagePlacementAbsorber.ImagePlacements[imageIndex].Image;
+
+            FileStream outputImage = new FileStream("temp-image.png", FileMode.Create);
+            xImage.Save(outputImage, ImageFormat.Png);
+            outputImage.Close();
+
+            FigureElement figureElement = taggedContent.CreateFigureElement();
+            figureElement.SetImage("temp-image.png");
+            figureElement.AlternativeText = "Aspose logo";
+
+            return figureElement;
+        }
+    }
+}
